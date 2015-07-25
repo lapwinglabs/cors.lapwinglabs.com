2c472078667676136f15387fe9e34f0230c279c4
Matthew Mueller <mattmuelle@gmail.com>
2015-07-25 13:14:02 -0700

    use throng

--- index.js
+++ index.js
@@ -4,15 +4,22 @@
 
 var cors_proxy = require('cors-anywhere');
 var port = process.env.PORT || 8080;
+var throng = require('throng');
 
 /**
  * Create a Server
  */
 
-cors_proxy.createServer({
+var server = cors_proxy.createServer({
   requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['cookie', 'cookie2']
-}).listen(port, function() {
-  var addr = this.address();
-  console.log('Listening on [%s]:%s', addr.address, addr.port);
-});
+})
+
+function listen () {
+  server.listen(port, function() {
+    var addr = this.address();
+    console.log('Listening on [%s]:%s', addr.address, addr.port);
+  });
+}
+
+throng(listen);
